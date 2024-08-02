103fb01621ed3a09d2973f4c457f489e63a7ceeb
Vaibhav Kolte <vaibhavkolte95@gmail.com>
2024-08-02 14:03:45 +0530

    starting properlly

--- JavaPrograms/src/Main.java
+++ JavaPrograms/src/Main.java
@@ -2,7 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Checking Git");
-
+        System.out.println("Doing Practice!! Best of Luck");
     }
 }
